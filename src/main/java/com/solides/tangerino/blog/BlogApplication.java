978ceb8a13762df711ec02a34977eed9f620aeb5
GabrielLimaBzr <gabriel18lb@outlook.com>
2024-01-26 16:09:19 -0300

    config spring security

--- src/main/java/com/solides/tangerino/blog/BlogApplication.java
+++ src/main/java/com/solides/tangerino/blog/BlogApplication.java
@@ -1,13 +1,20 @@
 package com.solides.tangerino.blog;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }
--- src/main/java/com/solides/tangerino/blog/model/entity/User.java
+++ src/main/java/com/solides/tangerino/blog/model/entity/User.java
@@ -1,22 +1,28 @@
 package com.solides.tangerino.blog.model.entity;
 
+import com.solides.tangerino.blog.model.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-@Table(name = "TB_USER")
+@Table(name = "TB_USERS")
 @Getter
 @Setter
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "INT_ID_USER", nullable = false)
     private Long id;
 
@@ -33,8 +39,45 @@ public class User {
     private String passphrase;
 
     @CreatedDate
-    @Column(name = "DT")
+    @Column(name = "DT_CREATED")
     private LocalDateTime created;
 
+    @Column(name = "INT_ROLE", nullable = false)
+    private UserRole role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Autorizacao simples não ha necessidade de verificar outras roles
+        return List.of(new SimpleGrantedAuthority("ROLE_DEFAULT"));
+    }
+
+    @Override
+    public String getPassword() {
+        return passphrase;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
